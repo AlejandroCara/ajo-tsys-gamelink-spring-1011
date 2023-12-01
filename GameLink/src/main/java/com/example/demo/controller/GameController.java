a5f800a347b51ad89e298d2c203263a17ed362d9
Alejandro <alejandro.carayol@gmail.com>
2023-12-01 12:45:42 +0100

    Paginación juegos terminada y testeada

--- GameLink/src/main/java/com/example/demo/controller/GameController.java
+++ GameLink/src/main/java/com/example/demo/controller/GameController.java
@@ -4,8 +4,13 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.Game;
@@ -26,8 +32,13 @@ public class GameController {
 	GameService gameService;
 
 	@GetMapping("/all")
-	public List<Game> listAllGames() {
-		return gameService.getAll();
+	public ResponseEntity<List<Game>> listAllGames(@RequestParam(defaultValue = "0") int page,
+													@RequestParam(defaultValue = "10") int size) {
+		Page<Game> departamentoPage = gameService.getPaginatedUsers(PageRequest.of(page, size));
+		List<Game> departamentoDTOs = departamentoPage.getContent().stream()
+				.collect(Collectors.toList());
+
+		return new ResponseEntity<>(departamentoDTOs, HttpStatus.OK);
 	}
 
 	@PostMapping("/add")
