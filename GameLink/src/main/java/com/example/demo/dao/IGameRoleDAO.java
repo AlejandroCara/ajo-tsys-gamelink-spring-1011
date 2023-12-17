7147725e7325cf1f3c6717366fd9ef81b59949c0
Alejandro <alejandro.carayol@gmail.com>
2023-12-17 18:29:34 +0100

    Filtro de game role por idgame y añadidas url de tag para user

--- GameLink/src/main/java/com/example/demo/dao/IGameRoleDAO.java
+++ GameLink/src/main/java/com/example/demo/dao/IGameRoleDAO.java
@@ -1,9 +1,17 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.dto.GameRole;
 
 public interface IGameRoleDAO extends JpaRepository<GameRole, Integer>{
-
+	
+	@Query(
+		value = "SELECT gr.* "
+			  + "FROM game_role as gr inner join game_game_role as ggr on  gr.id = ggr.id_game_role "
+			  + "WHERE ggr.id_game = ?1", nativeQuery = true)
+	List<GameRole> findGameRoleByGameId(int idGame);
 }
